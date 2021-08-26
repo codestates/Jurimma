5b0cfacc48ab62729092a7cd062a8a0659f8be1d
Youngseo Kang <youngseo.kangg@gmail.com>
2021-08-26 21:37:42 +0900

    :lipstick: [Client] App.js : 로고 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import "./app.css";
-import jurimma_logo from "./jurimma_logo.png";
 import dummyData from "./dummy/dummyData";
-// import Search from "./pages/Search"
+import Search from "./pages/Search"
 
 function App() {
   console.log(dummyData);
@@ -12,12 +11,12 @@ function App() {
 
       <div className="navBar">
         <header>
-          <h1 id="jurimma">
-            <img src={jurimma_logo} alt="logo" />
-          </h1>
+          <h1 id="jurimma"></h1>
         </header>
-        <section></section> {/* <Search /> 들어올 부분*/} 
-        <footer></footer>
+        <Search />
+        <footer>
+          copyright JURIMMA
+        </footer>
       </div>
     </div>
   );
