9216a3c33c75df2d35cf41723b6cefb31f43d7f5
Youngseo Kang <youngseo.kangg@gmail.com>
2021-08-26 21:04:37 +0900

    :lipstick: [Client] Search.js : SearchResult 값 여부에 따라 다른 디자인 구현

--- client/src/comp/SearchResult.js
+++ client/src/comp/SearchResult.js
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+import empty from "../empty.png"
 
 function SearchResult({data}){
-    let result = data.sort((a,b)=>a.thumbsup-b.thumbsup).reverse().slice(0,3)
-    // let result = []
+    // let result = data.sort((a,b)=>a.thumbsup-b.thumbsup).reverse().slice(0,3)
+    let result = []
     const ResultList = styled.ul`
         margin-top:20px;
         width:75%;
@@ -30,20 +31,34 @@ function SearchResult({data}){
             margin-top:0px;
         }
     `
+    const EmptyResult = styled.div`
+        text-align:center;
+        > img{
+            width:20vh;
+            height:20vh;
+        }
+    `
     return(
-        <ResultList>
-            {result.length===0? <p>검색 결과가 없습니다.</p>
+        <>
+        {result.length===0? 
+            <EmptyResult>
+                <img src={empty} alt="empty"/>
+                <p>검색 결과가 없습니다.</p>
+            </EmptyResult>
             :
-            result.map((ele, idx)=>{
-                return (
-                    <li key={idx}>
-                        <p>{ele.wordName}</p>
-                        <p>{ele.wordMean}</p>
-                        <p>{ele.thumbsup}</p>
-                    </li>
-                )
-            })}
-        </ResultList>
+            <ResultList>
+                {result.map((ele,idx)=>{
+                    return (
+                        <li key={idx}>
+                            <p>{ele.wordName}</p>
+                            <p>{ele.wordMean}</p>
+                            <p>{ele.thumbsup}</p>
+                        </li>
+                    )
+                })}
+            </ResultList>
+        }
+        </>
     )
 }
 
