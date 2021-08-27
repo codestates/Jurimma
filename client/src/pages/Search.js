d5db36863a9451e3b9b7ffb50eaa7b5aece79c0d
Lee Naeun <nanni4287@gmail.com>
2021-08-27 11:10:00 +0900

    :sparkles: [Client] searchResult.js : 검색했을 때 새로 만들기 버튼과 더보기 페이지로 가는 버튼 만들기

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,14 +1,14 @@
-import SearchInput from "../comp/SearchInput"
-import SearchResult from "../comp/SearchResult"
+import SearchInput from "../comp/SearchInput";
+import SearchResult from "../comp/SearchResult";
 import dummyData from "../dummy/dummyData";
 
-function Search (){
-    return(
-        <section>
-            <SearchInput />
-            <SearchResult data={dummyData.word}/>
-        </section>
-    )
+function Search() {
+  return (
+    <section>
+      <SearchInput />
+      <SearchResult data={dummyData.word} />
+    </section>
+  );
 }
 
 export default Search;
