23d0825086c4e22a3a8bbff7260b53644147a8fc
Lee Naeun <nanni4287@gmail.com>
2021-08-27 15:07:36 +0900

    :lipstick: [Client] Search.js : SearchInput.js / SearchResult.js css 수정

--- client/src/comp/SearchResult.js
+++ client/src/comp/SearchResult.js
@@ -1,65 +1,110 @@
 import React from "react";
 import styled from "styled-components";
-import empty from "../empty.png"
+import empty from "../empty.png";
 
-function SearchResult({data}){
-    // let result = data.sort((a,b)=>a.thumbsup-b.thumbsup).reverse().slice(0,3)
-    let result = []
-    const ResultList = styled.ul`
-        margin-top:20px;
-        width:75%;
-        margin: 0 auto;
-        display:flex;
-        flex-direction:column;
-        li{
-            display:flex;
-            height:8vh;
-            margin-top:2vh;
-            border:2px solid #000;
-            border-radius:40px;
-            text-align:center;
-            line-height:8vh;
-            background-color:#ffff8d;
-            p{
-                flex:1 1 auto
-            }
-            p:nth-child(2){
-                flex: 3 1 auto
-            }
-        }
-        li:nth-child(1){
-            margin-top:0px;
-        }
-    `
-    const EmptyResult = styled.div`
-        text-align:center;
-        > img{
-            width:20vh;
-            height:20vh;
-        }
-    `
-    return(
-        <>
-        {result.length===0? 
-            <EmptyResult>
-                <img src={empty} alt="empty"/>
-                <p>검색 결과가 없습니다.</p>
-            </EmptyResult>
-            :
-            <ResultList>
-                {result.map((ele,idx)=>{
-                    return (
-                        <li key={idx}>
-                            <p>{ele.wordName}</p>
-                            <p>{ele.wordMean}</p>
-                            <p>{ele.thumbsup}</p>
-                        </li>
-                    )
-                })}
-            </ResultList>
-        }
-        </>
-    )
+function SearchResult({ data }) {
+  let result = data
+    .sort((a, b) => a.thumbsup - b.thumbsup)
+    .reverse()
+    .slice(0, 3);
+  //let result = [];
+  const ResultList = styled.ul`
+    margin-top: 20px;
+    width: 65%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    li {
+      display: flex;
+      height: 8vh;
+      margin-top: 2vh;
+      border: 2px solid #000;
+      border-radius: 40px;
+      text-align: center;
+      line-height: 8vh;
+      background-color: #ffff8d;
+      p {
+        padding: 0 5px;
+        font-size: 75%;
+        flex: 1 0 auto;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      p:nth-child(2) {
+        flex: 3 1 auto;
+      }
+    }
+    li:nth-child(1) {
+      margin-top: 0px;
+    }
+  `;
+  const EmptyResult = styled.div`
+    text-align: center;
+    > img {
+      width: 20vh;
+      height: 20vh;
+    }
+  `;
+  const BtnWrap = styled.div`
+    width: 35vh;
+    margin: 0 auto;
+    margin-top: 2vh;
+    display: flex;
+    justify-content: space-evenly;
+  `;
+  const NewMoreBtn = styled.button`
+    width: 15vh;
+    height: 5vh;
+    border-radius: 5vh;
+    border: 2px solid black;
+    background-color: black;
+    color: white;
+    font-size: 0.8rem;
+    transition: 0.2s;
+    :hover {
+      background-color: white;
+      color: black;
+      cursor: pointer;
+    }
+  `;
+
+  return (
+    <>
+      {result.length === 0 ? (
+        <div>
+          <EmptyResult>
+            <img src={empty} alt="empty" />
+            <p>검색 결과가 없습니다.</p>
+          </EmptyResult>
+
+          <BtnWrap>
+            <NewMoreBtn>새로 만들기</NewMoreBtn>
+            <NewMoreBtn>더보기</NewMoreBtn>
+          </BtnWrap>
+        </div>
+      ) : (
+        <div>
+          <ResultList>
+            {result.map((ele, idx) => {
+              return (
+                <li key={idx}>
+                  <p>{ele.wordName}</p>
+                  <p>{ele.wordMean}</p>
+                  <p>{ele.thumbsup}</p>
+                </li>
+              );
+            })}
+          </ResultList>
+
+          <BtnWrap>
+            <NewMoreBtn>새로 만들기</NewMoreBtn>
+            <NewMoreBtn>더보기</NewMoreBtn>
+          </BtnWrap>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default SearchResult;
