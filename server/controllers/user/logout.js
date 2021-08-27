9a79d3190017ed3bdd4c40c6d30d104f2e8a61b3
minjman2659 <minjman2659@gmail.com>
2021-08-27 11:47:13 +0900

    [Server] logout.js : 로그아웃 기능 구현

--- server/controllers/user/logout.js
+++ server/controllers/user/logout.js
@@ -1,12 +1,18 @@
-const { user } = require("../../models");
 const { generateAccessToken, isAuthorized } = require("../tokenFunction/accessToken");
 const { generateRefreshToken, sendRefreshToken } = require("../tokenFunction/refreshToken");
 
 module.exports = {
     post: (req, res) => {
-        
-
-
-        // res.send("This is user/logout");
+        if(!isAuthorized(req)) {
+            res.status(400).json({ message: "You are not loged in!" });
+        } else {
+            const accessToken = generateAccessToken(null);
+            const refreshToken = generateRefreshToken(null);
+            sendRefreshToken(res, refreshToken);
+            res.status(200).json({
+                accessToken: accessToken,
+                message: "Success!"
+            });
+        }
     }
 }
