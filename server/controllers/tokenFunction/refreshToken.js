24ad4970fe9f3a39bfe27c48c8e184d407cc60b2
mniYUNSU <ownsgks@gmail.com>
2021-08-27 11:40:57 +0900

    [Server] signout.js, signup.js / 회원가입 및 탈퇴 API 구현

--- server/controllers/tokenFunction/refreshToken.js
+++ server/controllers/tokenFunction/refreshToken.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { user } = require("../../models");
 const { sign, verify } = require("jsonwebtoken");
 
 module.exports = {
@@ -7,22 +8,26 @@ module.exports = {
   },
   sendRefreshToken: (res, refreshToken) => {
     // TODO: JWT 토큰을 쿠키로 전달합니다.
-    res.cookie("jwt", refreshToken, {
+    res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: true,
       sameSite: "none",
     });
   },
-  isAuthorized: (req) => {
+  isAuthorized: async (req) => {
     // TODO: JWT 토큰 정보를 받아서 검증합니다.
-    const token = req.cookies;
+    const token = req.cookies.refreshToken;
     if (!token) {
-      return null;
+      return false;
     } else {
-      try {
-        return verify(token.jwt, process.env.REFRESH_SECRET);
-      } catch (error) {
-        return null;
+      const tokenCheck = verify(token, process.env.REFRESH_SECRET);
+      const findData = await user.findOne({
+        where: { id: tokenCheck.id },
+      });
+      if (!findData) {
+        return false;
+      } else {
+        return true;
       }
     }
   },
