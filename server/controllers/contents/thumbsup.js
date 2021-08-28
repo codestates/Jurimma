fe0814951d941502e00da851e2c5517618ceeb4b
mniYUNSU <ownsgks@gmail.com>
2021-08-28 15:07:29 +0900

    [Server] edit.js : 유저정보수정 구현

--- server/controllers/contents/thumbsup.js
+++ server/controllers/contents/thumbsup.js
@@ -8,37 +8,14 @@ const { verify } = require("jsonwebtoken");
 
 module.exports = {
   patch: async (req, res) => {
-    console.log(req.cookies);
-    try {
-      const contentsId = await req.body.contentsId;
-      const auth = await isAuthorized(req);
-      if (!auth) {
-        if (refreshAuthorized(req)) {
-          // accToken과 객체 같이 내보냄
-          const token = req.cookies.refreshToken;
-          const tokenCheck = verify(token, process.env.REFRESH_SECRET);
-          const accessToken = generateAccessToken(tokenCheck);
-          const dbContent = await content.findOne({
-            where: { id: contentsId },
-          });
-          await dbContent.increment("thumbsup");
-          const updatedContent = await content.findOne({
-            where: { id: contentsId },
-          });
-          await thumbs.create({
-            user_Id: tokenCheck.id,
-            content_Id: contentsId,
-          });
-          res.status(200).json({ accessToken, updatedContent });
-        } else {
-          // 로그아웃하고 메인페이지로
-          res.status(401).json({ message: "Send new Login Request" });
-        }
-      } else {
-        // 응답 200
-        const accToken = req.headers.authorization;
-        const realToken = accToken.split(" ")[1];
-        const userData = verify(realToken, process.env.ACCESS_SECRET);
+    const contentsId = await req.body.contentsId;
+    const auth = await isAuthorized(req);
+    if (!auth) {
+      if (refreshAuthorized(req)) {
+        // accToken과 객체 같이 내보냄
+        const token = req.cookies.refreshToken;
+        const tokenCheck = verify(token, process.env.REFRESH_SECRET);
+        const accessToken = generateAccessToken(tokenCheck);
         const dbContent = await content.findOne({
           where: { id: contentsId },
         });
@@ -46,12 +23,29 @@ module.exports = {
         const updatedContent = await content.findOne({
           where: { id: contentsId },
         });
-        await thumbs.create({ user_Id: userData.id, content_Id: contentsId });
-        res.status(200).json({ data: updatedContent });
+        await thumbs.create({
+          user_Id: tokenCheck.id,
+          content_Id: contentsId,
+        });
+        res.status(200).json({ accessToken, updatedContent });
+      } else {
+        // 로그아웃하고 메인페이지로
+        res.status(401).json({ message: "Send new Login Request" });
       }
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: "Server Error" });
+    } else {
+      // 응답 200
+      const accToken = req.headers.authorization;
+      const realToken = accToken.split(" ")[1];
+      const userData = verify(realToken, process.env.ACCESS_SECRET);
+      const dbContent = await content.findOne({
+        where: { id: contentsId },
+      });
+      await dbContent.increment("thumbsup");
+      const updatedContent = await content.findOne({
+        where: { id: contentsId },
+      });
+      await thumbs.create({ user_Id: userData.id, content_Id: contentsId });
+      res.status(200).json({ data: updatedContent });
     }
   },
 };
