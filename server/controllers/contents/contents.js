6c042cd107728bee96023a6fa21a88b96a48215a
minjman2659 <minjman2659@gmail.com>
2021-08-28 13:54:23 +0900

    Not Yet

--- server/controllers/contents/contents.js
+++ server/controllers/contents/contents.js
@@ -4,6 +4,11 @@ module.exports = {
         res.send("This is contents_new(POST)");
     },
 
+    delete: (req, res) => {
+        
+        res.send("This is contents_delete(DELETE)");
+    },
+
     patch: (req, res) => {
         res.send("This is contents_edit(PATCH)")
     }
--- server/routes/contents.js
+++ server/routes/contents.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { contentsController } = require("../controllers");
 
 router.post("/", contentsController.contents.post);
+router.delete("/", contentsController.contents.delete);
 router.patch("/", contentsController.contents.patch);
 router.patch("/thumbsup", contentsController.thumbsup.patch);
 
