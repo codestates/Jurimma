2c84ea703aa6fd133f0e3c3f0afaee6e88ea558f
mniYUNSU <ownsgks@gmail.com>
2021-08-29 17:58:22 +0900

    [Server] 환경변수 변경

--- server/config/config.js
+++ server/config/config.js
@@ -2,27 +2,24 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DATABASE_USERNAME,
+    username: "root",
     password: process.env.DATABASE_PASSWORD,
     database: "jurimma_development",
-    host: process.env.DATABASE_HOST,
+    host: "localhost",
     dialect: "mysql",
-    port: process.env.DATABASE_PORT,
   },
   test: {
-    username: process.env.DATABASE_USERNAME,
+    username: "root",
     password: process.env.DATABASE_PASSWORD,
     database: "jurimma_test",
-    host: process.env.DATABASE_HOST,
+    host: "localhost",
     dialect: "mysql",
-    port: process.env.DATABASE_PORT,
   },
   production: {
-    username: process.env.DATABASE_USERNAME,
+    username: "root",
     password: process.env.DATABASE_PASSWORD,
     database: "jurimma_production",
-    host: process.env.DATABASE_HOST,
+    host: "localhost",
     dialect: "mysql",
-    port: process.env.DATABASE_PORT,
   },
 };
