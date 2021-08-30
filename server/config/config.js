aa969789b03901de2e5dc928f058fe6f266fbf9d
mniYUNSU <ownsgks@gmail.com>
2021-08-30 18:18:21 +0900

    [MASTER] 완전체

--- server/config/config.js
+++ server/config/config.js
@@ -2,26 +2,26 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DATABASE_USERNAME,
+    username: process.env.DATABASE_USERNAME || "root",
     password: process.env.DATABASE_PASSWORD,
     database: "jurimma_development",
-    host: process.env.DATABASE_HOST,
+    host: process.env.DATABASE_HOST || "localhost",
     dialect: "mysql",
     port: process.env.DATABASE_PORT,
   },
   test: {
-    username: process.env.DATABASE_USERNAME,
+    username: process.env.DATABASE_USERNAME || "root",
     password: process.env.DATABASE_PASSWORD,
     database: "jurimma_test",
-    host: process.env.DATABASE_HOST,
+    host: process.env.DATABASE_HOST || "localhost",
     dialect: "mysql",
     port: process.env.DATABASE_PORT,
   },
   production: {
-    username: process.env.DATABASE_USERNAME,
+    username: process.env.DATABASE_USERNAME || "root",
     password: process.env.DATABASE_PASSWORD,
     database: "jurimma_production",
-    host: process.env.DATABASE_HOST,
+    host: process.env.DATABASE_HOST || "localhost",
     dialect: "mysql",
     port: process.env.DATABASE_PORT,
   },
